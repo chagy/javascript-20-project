1d53c43736d722466576018e4f10a9ae924673bb
chagy <chagy_x_@hotmail.com>
2022-01-17 13:46:43 +0700

    167

--- project-16-Drag-Drop/script.js
+++ project-16-Drag-Drop/script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll(".solid");
 const addItemContainers = document.querySelectorAll(".add-container");
 const addItems = document.querySelectorAll(".add-item");
 // Item Lists
-const itemLists = document.querySelectorAll(".drag-item-list");
+const listColumns = document.querySelectorAll(".drag-item-list");
 const backlogList = document.getElementById("backlog-list");
 const progressList = document.getElementById("progress-list");
 const completeList = document.getElementById("complete-list");
@@ -20,6 +20,8 @@ let onHoldListArray = [];
 let listArrays = [];
 
 // Drag Functionality
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -68,6 +70,8 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement("li");
   listEl.classList.add("drag-item");
   listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute("ondragstart", "drag(event)");
 
   columnEl.appendChild(listEl);
 }
@@ -99,6 +103,29 @@ function updateDOM() {
     createItemEl(onHoldList, 0, backlogItem, index);
   });
   // Run getSavedColumns only once, Update Local Storage
+}
+
+function drag(e) {
+  draggedItem = e.target;
+}
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function dragEnter(column) {
+  listColumns[column].classList.add("over");
+  currentColumn = column;
+}
+
+function drop(e) {
+  e.preventDefault();
+  listColumns.forEach((column) => {
+    column.classList.remove("over");
+  });
+
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
 }
 
 updateDOM();
