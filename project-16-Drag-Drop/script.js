be59f2d8db34a720d2a5277eb2bbaf157a27a16f
chagy <chagy_x_@hotmail.com>
2022-01-17 13:53:33 +0700

    168

--- project-16-Drag-Drop/script.js
+++ project-16-Drag-Drop/script.js
@@ -103,6 +103,33 @@ function updateDOM() {
     createItemEl(onHoldList, 0, backlogItem, index);
   });
   // Run getSavedColumns only once, Update Local Storage
+
+  updatedOnLoad = true;
+  updateSavedColumns();
+}
+
+function rebuildArrays() {
+  backlogListArray = [];
+  for (let i = 0; i < backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+
+  completeListArray = [];
+  for (let i = 0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+
+  updateDOM();
 }
 
 function drag(e) {
